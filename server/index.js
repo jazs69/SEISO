8eb9c4ceb65d4637c1a4a3aa8034168d00695caf
Jayesh Yadav <jayesh10723@gmail.com>
2025-05-17 21:45:06 +0530

    Update index.js | corse origin .env

--- server/index.js
+++ server/index.js
@@ -22,7 +22,7 @@ connectDB()
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: process.env.FRONTEND_URL || "http://localhost:5173",
     credentials: true, // Allow cookies to be sent
   })
 );
